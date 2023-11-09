2df91da10599ca75071d1e298be4b8f8f27f724e
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-11-09 15:36:19 -0300

    adding validations

--- pkg/models/user.go
+++ pkg/models/user.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 type User struct {
 	Id        uint64    `json:"id,omitempty"`
@@ -9,4 +13,38 @@ type User struct {
 	Email     string    `json:"email,omitempty"`
 	Password  string    `json:"password,omitempty"`
 	CreatedAt time.Time `json:"created_at,omitempty"`
+}
+
+func (u *User) Prepare() error {
+	if err := u.validate(); err != nil {
+		return err
+	}
+	u.format()
+	return nil
+}
+
+func (u *User) validate() error {
+	if u.Name == "" {
+		return errors.New("the name field is required")
+	}
+
+	if u.Nick == "" {
+		return errors.New("the nick field is required")
+	}
+
+	if u.Email == "" {
+		return errors.New("the email field is required")
+	}
+
+	if u.Password == "" {
+		return errors.New("the password field is required")
+	}
+
+	return nil
+}
+
+func (u *User) format() {
+	u.Name = strings.TrimSpace(u.Name)
+	u.Nick = strings.TrimSpace(u.Nick)
+	u.Email = strings.TrimSpace(u.Email)
 }
