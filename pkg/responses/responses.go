a4bf2ec9656ae561f7797f3780ac8a0e9cac707f
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-11-09 16:35:37 -0300

    adding content-type header

--- pkg/responses/responses.go
+++ pkg/responses/responses.go
@@ -7,6 +7,7 @@ import (
 )
 
 func Json(w http.ResponseWriter, status int, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	if err := json.NewEncoder(w).Encode(data); err != nil {
 		log.Fatal(err)
