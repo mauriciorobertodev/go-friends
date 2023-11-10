c3ca08d4a004658f50df7b9111620fe0355b6000
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-11-10 14:49:06 -0300

    validating token

--- pkg/authentication/token.go
+++ pkg/authentication/token.go
@@ -1,7 +1,11 @@
 package authentication
 
 import (
+	"errors"
+	"fmt"
 	"go-friends/pkg/config"
+	"net/http"
+	"strings"
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
@@ -16,4 +20,37 @@ func CreateToken(id uint64) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, permissions)
 
 	return token.SignedString(config.SecretKey)
+}
+
+func ValidateToken(r *http.Request) error {
+	tokenString := extractToken(r)
+	token, err := jwt.Parse(tokenString, getVerifyKey)
+
+	if err != nil {
+		return err
+	}
+
+	if _, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return nil
+	}
+
+	return errors.New("invalid token")
+}
+
+func extractToken(r *http.Request) string {
+	token := r.Header.Get("Authorization")
+
+	if len(strings.Split(token, " ")) == 2 {
+		return strings.Split(token, " ")[1]
+	}
+
+	return ""
+}
+
+func getVerifyKey(token *jwt.Token) (interface{}, error) {
+	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+		return nil, fmt.Errorf("unexpected signature method: %v", token.Header["alg"])
+	}
+
+	return config.SecretKey, nil
 }
